04fce1c37338f4f5e860d835bc9b006ebc77c456
wojunfeng <wjun1924@gmail.com>
2015-10-22 10:27:06 -0400

    updated tab bar icon color

--- BudgetAPP/TabBarViewController.m
+++ BudgetAPP/TabBarViewController.m
@@ -33,23 +33,23 @@
     
     for (UIViewController *viewController in self.viewControllers) {
         if ([viewController isKindOfClass:(SettingsViewController.class)]) {
-                UIImage *icon = [IonIcons imageWithIcon:@"\uf411"
-                                              iconColor:[UIColor colorWithRed:0.225 green:0.1965 blue:1.0 alpha:1.0]
+            UIImage *icon = [IonIcons imageWithIcon:ion_ios_gear
+                                              iconColor:[UIColor colorWithRed:0.3216 green:0.749 blue:1.0 alpha:1.0]
                                                iconSize:37.0f
                                               imageSize:CGSizeMake(90.0f, 90.0f)];
             UITabBarItem *tabIcon = [[UITabBarItem alloc] initWithTitle:@"Settings" image:icon tag:0];
             viewController.tabBarItem = tabIcon;
             
         } else if ([viewController isKindOfClass:(ExpensesViewController.class)]){
-            UIImage *icon = [IonIcons imageWithIcon:ion_social_usd_outline
-                                          iconColor:[UIColor colorWithRed:0.225 green:0.1965 blue:1.0 alpha:1.0]
+            UIImage *icon = [IonIcons imageWithIcon:ion_card
+                                          iconColor:[UIColor colorWithRed:0.3216 green:0.749 blue:1.0 alpha:1.0]
                                            iconSize:37.0f
                                           imageSize:CGSizeMake(90.0f, 90.0f)];
             UITabBarItem *tabIcon = [[UITabBarItem alloc] initWithTitle:@"Expenses" image:icon tag:0];
             viewController.tabBarItem = tabIcon;
         } else {
-            UIImage *icon = [IonIcons imageWithIcon:ion_ios_paper_outline
-                                          iconColor:[UIColor colorWithRed:0.225 green:0.1965 blue:1.0 alpha:1.0]
+            UIImage *icon = [IonIcons imageWithIcon:ion_ios_paper
+                                          iconColor:[UIColor colorWithRed:0.3216 green:0.749 blue:1.0 alpha:1.0]
                                            iconSize:37.0f
                                           imageSize:CGSizeMake(90.0f, 90.0f)];
             UITabBarItem *tabIcon = [[UITabBarItem alloc] initWithTitle:@"Summary" image:icon tag:0];
